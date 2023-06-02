6c1772a24e2abd19ab893f41d26d1e531b0b809f
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2023-06-02 00:30:39 -0500

    Update Project_1.py

--- Day 1/Project_1.py
+++ Day 1/Project_1.py
@@ -1,5 +1,5 @@
-
 # Band Name Generator
-
-name = input("What is your name: ")
-print(name)
+print("welcome to the Band Name Generator.")
+city = (input("What's the name of the city you grew up in? "))
+pet = (input("What's your pet's name? "))
+print("Your band name could be " + city + " " + pet)
