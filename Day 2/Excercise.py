bc274f2ebba7d85120aaebb046aabc3b124623ca
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2023-12-06 00:22:29 -0600

    Update Excercise.py

--- Day 2/Excercise.py
+++ Day 2/Excercise.py
@@ -1,1 +1,20 @@
-Comming Soon!
+'''
+Write a program that checks if the water is boiling.
+
+Take the integer temperature in Celsius as input and output "Boiling" if the temperature is above or equal to 100.
+
+Sample Input
+
+105
+
+Sample Output
+
+Boiling
+'''
+
+temp = int(input())
+
+if temp >=100:
+    print("Boiling")
+else:
+    print()
