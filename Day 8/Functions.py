f9985dc28e506102cd635aac6ff9ef5fe4774d33
Oyegun Austine Augustine <115058054+Jawonlaya@users.noreply.github.com>
2023-12-20 23:55:37 -0600

    Update and rename Excercise.py to Functions.py

---
+++ Day 8/Functions.py
@@ -0,0 +1,14 @@
+
+''' Basic function
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print("f How are you doing today {name}")
+greet_with_name("Jack")
+'''
+
+''' Function with more than 1 input
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}")
+greet_with(f"Jack" "Nowhere")
+'''
